36ea1e4db51e0938f53064f8f09b7bff707ed4fa
KIRAN969 <rachapallekirankumarreddy@gmail.com>
2021-08-06 16:41:52 +0530

    day14

--- 5august-day14assignments/5-Aug-2021-kiran-assessment/multiprocess.py
+++ 5august-day14assignments/5-Aug-2021-kiran-assessment/multiprocess.py
@@ -1,33 +1,37 @@
-import multiprocessing,time
-def Primenumbers():
-    for num in range(2,500):
-        time.sleep(1)
-        for i in range(2,num):
-            if(num%i)==0:
-                break
-        else:
-            print(num,end=' ')
+import multiprocessing,logging
+try:
+    def Primenumbers():
+        for num in range(2,500):
+            #time.sleep(1)
+            for i in range(2,num):
+                if(num%i)==0:
+                    break
+            else:
+                print(num,end=' ')
 
-def Palindromenumbers():
-    for num in range(10,500):
-        time.sleep(1)
-        temp=num
-        reverse=0
-        while(temp>0):
-            rem=temp%10
-            reverse=(reverse*10)+rem
-            temp=temp//10
-        if(num==reverse):
-            print(num,end=' ')
+    def Palindromenumbers():
+        for num in range(10,500):
+            #time.sleep(1)
+            temp=num
+            reverse=0
+            while(temp>0):
+                rem=temp%10
+                reverse=(reverse*10)+rem
+                temp=temp//10
+            if(num==reverse):
+                print(num,end=' ')
 
 
-if(__name__=='__main__'):
-    
-    p1=multiprocessing.Process(target=Primenumbers)
-    p2=multiprocessing.Process(target=Palindromenumbers)
-    p1.start()
-    p1.join()
-    print(",,,,")
-    p2.start()
-    p2.join()
-    print(",,,,,")
+    if(__name__=='__main__'):
+        
+        p1=multiprocessing.Process(target=Primenumbers)
+        p2=multiprocessing.Process(target=Palindromenumbers)
+        p1.start()
+        p2.start()
+        p1.join()
+        p2.join()
+        print(",,,,,")
+except Exception:
+    logging.error("something went wrong")
+finally:
+    print("completed")
--- 5august-day14assignments/5-Aug-2021-kiran-assessment/multithreading1.py
+++ 5august-day14assignments/5-Aug-2021-kiran-assessment/multithreading1.py
@@ -1,4 +1,5 @@
-import threading,time
+import threading,time,logging
+logging.basicconfig(filename='execution.log',level=logging.INFO)
 def Primenumbers():
     for num in range(2,500):
         time.sleep(2)
@@ -19,12 +20,17 @@ def Palindromenumbers():
             temp=temp//10
         if(num==reverse):
             print(num,end=' ')
+if(__name__=='__main__'):
 
 t1=threading.Thread(target=Primenumbers)
 t2=threading.Thread(target=Palindromenumbers)
 t1.start()
-t1.join()
-print("......")
 t2.start()
+t1.join()
 t2.join()
 print(".........")
+try:
+    logging.info("prime and palindrome numbers are printed")
+except:
+    logging.error("an unknown error occured")  
+
